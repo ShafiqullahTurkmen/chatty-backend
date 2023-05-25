b036c9d04fb290f5222e74a327453430312abaed
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-05-25 07:41:34 +0300

    first commit

---
+++ src/setupDatabase.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import Logger from 'bunyan';
+import { config } from './config';
+
+const log: Logger = config.createLoggter('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => log.info('Successfully Connectted To DB'))
+      .catch((e) => {
+        log.error(`Error connecting to database ${e}`);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnect', connect);
+};
